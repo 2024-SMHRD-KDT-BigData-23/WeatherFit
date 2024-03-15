81b23fb7f22bf4e0318bb7bb01b86b435a226f48
YeonDoIt <duswo5170@gmail.com>
2024-03-15 12:19:07 +0900

    첫커밋

---
+++ WeatherFit/src/main/java/com/smhrd/database/DAO.java
@@ -0,0 +1,14 @@
+package com.smhrd.database;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.smhrd.model.MemberVO;
+
+public class DAO {
+
+private SqlSessionFactory factory = MySqlSessionManager.getSqlSessionFactory();
+	
+	
+	
+}
---
+++ WeatherFit/src/main/java/com/smhrd/model/UserVO.java
@@ -0,0 +1,143 @@
+package com.smhrd.model;
+
+//회원 
+public class UserVO {
+
+ // 회원 아이디 
+ private String userId;
+
+ // 회원 비밀번호 
+ private String userPw;
+
+ // 회원 이름 
+ private String userName;
+
+ // 회원 성별 
+ private String userGender;
+
+ // 회원 닉네임 
+ private String userNick;
+
+ // 회원 지역 
+ private String userRegion;
+
+ // 회원 가입일자 
+ private BigDecimal joinedAt;
+ 
+ // 회원 키 
+ private BigDecimal userHeight;
+
+ // 회원 몸무게 
+ private BigDecimal userWeight;
+
+ // 회원 프로필이미지 
+ private String userProfileImg;
+
+ // 회원 프로필소개 
+ private String userProfileInfo;
+
+ public String getUserId() {
+     return userId;
+ }
+
+ public void setUserId(String userId) {
+     this.userId = userId;
+ }
+
+ public String getUserPw() {
+     return userPw;
+ }
+
+ public void setUserPw(String userPw) {
+     this.userPw = userPw;
+ }
+
+ public String getUserName() {
+     return userName;
+ }
+
+ public void setUserName(String userName) {
+     this.userName = userName;
+ }
+
+ public String getUserGender() {
+     return userGender;
+ }
+
+ public void setUserGender(String userGender) {
+     this.userGender = userGender;
+ }
+
+ public String getUserNick() {
+     return userNick;
+ }
+
+ public void setUserNick(String userNick) {
+     this.userNick = userNick;
+ }
+
+ public String getUserRegion() {
+     return userRegion;
+ }
+
+ public void setUserRegion(String userRegion) {
+     this.userRegion = userRegion;
+ }
+
+ public Timestamp getJoinedAt() {
+     return joinedAt;
+ }
+
+ public void setJoinedAt(Timestamp joinedAt) {
+     this.joinedAt = joinedAt;
+ }
+
+ public BigDecimal getUserHeight() {
+     return userHeight;
+ }
+
+ public void setUserHeight(BigDecimal userHeight) {
+     this.userHeight = userHeight;
+ }
+
+ public BigDecimal getUserWeight() {
+     return userWeight;
+ }
+
+ public void setUserWeight(BigDecimal userWeight) {
+     this.userWeight = userWeight;
+ }
+
+ public String getUserProfileImg() {
+     return userProfileImg;
+ }
+
+ public void setUserProfileImg(String userProfileImg) {
+     this.userProfileImg = userProfileImg;
+ }
+
+ public String getUserProfileInfo() {
+     return userProfileInfo;
+ }
+
+ public void setUserProfileInfo(String userProfileInfo) {
+     this.userProfileInfo = userProfileInfo;
+ }
+
+ // tbUser 모델 복사
+ public void CopyData(tbUser param)
+ {
+     this.userId = param.getUserId();
+     this.userPw = param.getUserPw();
+     this.userName = param.getUserName();
+     this.userGender = param.getUserGender();
+     this.userNick = param.getUserNick();
+     this.userRegion = param.getUserRegion();
+     this.joinedAt = param.getJoinedAt();
+     this.userHeight = param.getUserHeight();
+     this.userWeight = param.getUserWeight();
+     this.userProfileImg = param.getUserProfileImg();
+     this.userProfileInfo = param.getUserProfileInfo();
+ }
+ 
+}
