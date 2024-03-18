519ea43f8b64281349c20edf19d13211cbe7d12f
이동주 <spotiz3377@naver.com>
2024-03-18 11:07:26 +0900

    dong 브랜치 생성

--- WeatherFit/src/main/java/com/smhrd/database/DAO.java
+++ WeatherFit/src/main/java/com/smhrd/database/DAO.java
@@ -3,8 +3,6 @@ package com.smhrd.database;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
-import com.smhrd.model.MemberVO;
-
 public class DAO {
 
 private SqlSessionFactory factory = MySqlSessionManager.getSqlSessionFactory();
