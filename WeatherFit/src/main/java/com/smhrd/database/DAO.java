ab29502658ef80cfa40d200eeb53db18903f3f17
김동현 <109597157+dohdark@users.noreply.github.com>
2024-03-18 10:40:11 +0900

    dao.java import memberVO 삭제

--- WeatherFit/src/main/java/com/smhrd/database/DAO.java
+++ WeatherFit/src/main/java/com/smhrd/database/DAO.java
@@ -3,7 +3,6 @@ package com.smhrd.database;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
-import com.smhrd.model.MemberVO;
 
 public class DAO {
 
